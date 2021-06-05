efb12187f5fcc76378b336fb592bde4a7b374aaa
Danfly29 <daniel.soto.j@gmail.com>
2021-06-05 01:13:28 -0600

    gmEr

--- statisticalutils.py
+++ statisticalutils.py
@@ -5,10 +5,23 @@ class SomethingValueStatistics:
         """Initialize name and weight of a stock or portfolio"""
         self.weight=weight
         self.name=name
-    def calc_expected_return(self,series):
+    def get_return_series(self,series):
+        """Takes a Date Price Series and uses two mostly identical data frames
+        two move indexes around placing today and next day prices side by side.
+        Finally creating a series attribute."""
         df = pd.DataFrame(series).reset_index()
         copy = df
         copy['Numerator'] =copy['Adj Close']
-        copy = copy.drop([0]).set_index(['Date']).reset_index()
+        copy = copy.drop([0])
+        copy = copy.set_index('Date').reset_index()
         df['Numerator']=copy['Numerator']
-        df["% Change"]=df['Numerator']/df['Adj Close']
+        copy["% Change"]=df['Numerator']/df['Adj Close']
+        self.returnseries = copy.set_index('Date')['% Change']
+
+    def calc_observed_return(self,series):
+        """Uses return series to get geometric mean of returns"""
+        self.get_return_series(series)
+        prod = self.returnseries.prod()
+        size=len(self.returnseries)
+        geometric_mean_return=prod**(1/size)
+        return(geometric_mean_return)
